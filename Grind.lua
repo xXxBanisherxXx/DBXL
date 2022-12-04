81fd5a2ad8378b1d5a0889ad9a90c7f40f39e19c
xXxBanisherxXx <93361925+xXxBanisherxXx@users.noreply.github.com>
2022-12-04 12:27:45 -0500

    Update Grind.lua

--- Grind.lua
+++ Grind.lua
@@ -133,3 +133,10 @@ if game.PlaceId == game.PlaceId then
 
 	})
 end
+
+local queue_on_teleport =
+queue_on_teleport or
+    syn and
+        syn.queue_on_teleport [[
+       repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/xXxBanisherxXx/DBXL/main/Grind.lua'))()]]
