6d50b125e9b6c3cb7221480f1ad7501955c50cdd
xXxBanisherxXx <93361925+xXxBanisherxXx@users.noreply.github.com>
2022-12-05 06:50:47 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -58,7 +58,7 @@ end
 if game.PlaceId == 1186465433 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/xXxBanisherxXx/Scripts/main/DBXL.lua"))()
 else
-	print('Game no found')
+	Player:Kick('Game no found')
 end
 
 SendWebhook()
