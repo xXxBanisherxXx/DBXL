36f5d201b903a629b04def7dfeeec32695a3389d
xXxBanisherxXx <93361925+xXxBanisherxXx@users.noreply.github.com>
2022-12-04 19:07:51 -0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+function Load(File)
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/xXxBanisherxXx/Scripts/main/'..File));
+	return;
+end
+
+if game.PlaceId == 1186465433 then
+	Load('DBXL')
+end
