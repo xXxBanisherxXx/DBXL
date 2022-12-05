2b8397e78715484354e475e9a7b32f0a48595cdd
xXxBanisherxXx <93361925+xXxBanisherxXx@users.noreply.github.com>
2022-12-04 19:29:59 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 function Load(File)
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/xXxBanisherxXx/Scripts/main/'..File..'.lua'));
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/xXxBanisherxXx/Scripts/main/'..File));
 	return;
 end
 
 
 if game.PlaceId == 1186465433 then
-	Load('DBXL')
+	Load('DBXL.lua')
 end
